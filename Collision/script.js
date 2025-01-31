bd0e91ca6b15e433d224be0afedbc49ec0e8595a
MakaRonish <ronishmakaju12345@gmail.com>
2025-01-30 21:04:14 -0500

    pointing pixel

---
+++ Collision/script.js
@@ -0,0 +1,37 @@
+var rect1 = { x: 5, y: 5, width: 50, height: 50 }
+var rect2 = { x: 20, y: 10, width: 10, height10 }
+
+//one way
+if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
+
+    //collision
+} else {
+    //nocollision
+}
+//another one way
+if (rect1.x > rect2.x + rect2.width || rect1.x + rect1.width < rect2.x || rect1.y > rect2.y + rect2.height || rect1.y + rect1.height < rect2.y) {
+
+    //no collision
+} else {
+    //collision
+}
+
+
+//circle
+
+const circle1 = { x: 10, y: 10, radius: 300 };
+const circle2 = { x: 500, y: 500, radius: 150 };
+
+let dx = circle2.x - circle1.x;
+let dy = circle2.y - circle1.y;
+
+let distance = Math.sqrt(dx * dx + dy * dy)
+let sumofradi = circle1.radius + circle2.radius
+
+if (distance < sumofradi) {
+    //circle collide
+} else if (distance === sumofradi) {
+    //touch
+} else if (distance > sumofradi) {
+    //no colide
+}
---
+++ Movement copy 2/script.js
@@ -0,0 +1,75 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGHT = canvas.height = 700;
+const numberOfEnemies = 10;
+const enemiesArray = []
+
+// const enemyImage = new Image();
+// enemyImage.src = 'enemies/enemy1.png'
+let gameFrame = 0;
+
+// enemy1 = {
+//     x: 0,
+//     y: 0,
+//     width: 200,
+//     height: 200,
+// }
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemies/enemy3.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 1;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 1.5 + 0.5;
+        this.curve = Math.random() * 200 + 50;
+
+
+    }
+    update() {
+        this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 90) + (CANVAS_WIDTH / 2 - this.width / 2);
+        this.y = canvas.height / 2 * Math.cos(this.angle * Math.PI / 180) + (CANVAS_HEIGHT / 2 - this.height / 2);
+        if (this.x + this.width < 0) this.x = canvas.width;
+        this.angle += this.angleSpeed;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        // ctx.strokeRect(this.x, this.y, this.width, this.height)
+
+
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+
+
+    }
+}
+
+for (let i = 0; i <= numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+
+}
+console.log(enemiesArray)
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    enemiesArray.forEach((enemy) => {
+        enemy.update()
+        enemy.draw()
+    })
+    gameFrame++;
+
+
+
+    requestAnimationFrame(animate)
+}
+animate()
